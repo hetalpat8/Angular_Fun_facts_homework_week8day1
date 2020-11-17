05c1d76fe8a033cd7de8f3e99f748e5883be4b13
hetalpat8 <hetalpat8@gmail.com>
2020-11-17 07:57:06 -0600

    fun facts angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
+import { PromisesComponent } from './promises/promises.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { MapComponent } from './map/map.component';
+import { CreatorComponent } from './creator/creator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'main',
+    component: MainPageComponent
+  },
+  {
+  
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: 'promises',
+    component: PromisesComponent
+  },
+  {
+    path: 'callbacks',
+    component: CallbacksComponent
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+  {
+    path: 'creator',
+    component: CreatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
+import { PromisesComponent } from './promises/promises.component';
+import { CallbacksComponent } from './callbacks/callbacks.component';
+import { MapComponent } from './map/map.component';
+import { CreatorComponent } from './creator/creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginComponent,
+    PromisesComponent,
+    CallbacksComponent,
+    MapComponent,
+    CreatorComponent
   ],
   imports: [
     BrowserModule,
